14251d516c50471ba19a3edc5c8e4676b8edb979
Minyewoo <const.trushov@gmail.com>
2019-07-21 16:29:59 +0500

    fix

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,6 +3,7 @@ import snapshotReducer from './snapshotReducer'
 import {combineReducers} from 'redux'
 import { firestoreReducer } from 'redux-firestore'
 import { firebaseReducer } from 'react-redux-firebase'
+
 const rootReducer = combineReducers({
     auth: authReducer,
     snapshot: snapshotReducer,
