8776cf1d5a27497f1c808eb8c76e55458350bf0b
Minyewoo <const.trushov@gmail.com>
2019-08-04 22:57:56 +0500

    delete animation code x2

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,13 +4,12 @@ import { connect } from 'react-redux'
 import { signOut } from '../../store/actions/authActions'
 
 const SignedInLinks = (props) => {
-    var links = <ul className="navbar-buttons">
-                    <li className="active-buttons" id="btn1"><NavLink to='/create'>New snapshot</NavLink></li>
+    return (
+        <ul className="navbar-buttons">
+                    <li className="active-buttons" id="btn1" ><NavLink to='/create'>New snapshot</NavLink></li>
                     <li className="active-buttons" ><a onClick={props.signOut}>Sign out</a></li>
                     <li className="active-buttons" id="btn2"><NavLink to='/'><div className='btn btn-floating pink lighten-1'>{props.profile.initials}</div></NavLink></li>
-                </ul>;
-    return (
-        {links}
+                </ul>
     )
 }
 
